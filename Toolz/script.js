454d1b9f71139a901011a8c3859e54067ccd2d16
Kallyan <singhakallyan@gmail.com>
2021-01-31 01:02:01 +0530

    CSS changed

--- Toolz/script.js
+++ Toolz/script.js
@@ -1,5 +1,5 @@
 function openmenu() {
-    document.getElementById("nav-bar").style.opacity = "100%";
+    document.getElementById("nav-bar").style.width = "70%";
     document.getElementById("nav-bar").style.animation = "mynav 1.0s ease";
     document.getElementById("nav-bar").style.display = "block";
     document.getElementById("sico").style.display = "flex";
@@ -8,18 +8,12 @@ function openmenu() {
 }
 
 function closemenu() {
-    document.getElementById("nav-bar").style.opacity = "0%";
+    document.getElementById("nav-bar").style.width = "0%";
     document.getElementById("nav-bar").style.display = "none";
     document.getElementById("nav-bar").style.animation = "closemenu 2.8s ease ";
     document.getElementById("sico").style.display = "none";
     document.getElementById("bdy").style.overflow = "visible";
 }
-
-function closelog() {
-    document.getElementById("popbox1").style.display = "none";
-
-}
-
 function loading() {
     document.getElementById("load").style.display = "none";
 }
