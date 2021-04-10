c9d77af05710ccfa15e6ec957797bd74fc0a2db2
Kallyan01 <singhakallyan@gmail.com>
2021-04-10 07:56:32 +0530

    added js calc() function to the web app

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function calc() {
+  var total;
+  var ammo = document.getElementById("principal").value;
+  console.log(ammo);
+  var intrate = document.getElementById("rate").value;
+  console.log(intrate);
+  var year = document.getElementById("years").value;
+  console.log(year);
+  total = ((ammo / 100) * intrate) * year;
+  var curryr = 2021;
+   var tyear = parseInt(curryr)+parseInt(year);
+  console.log(tyear);
+  document.getElementById("result").innerHTML = "If you deposit\t" + ammo + "\tat an interest rate of\t" + intrate + "%" + "\tyou will receive an ammount of\t" + total + "\tin the year\t" + tyear;
 }
-        
